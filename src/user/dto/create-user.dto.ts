2d2930aa311260bcc35d4e6ae89fb8cf78c7c658
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2025-09-19 16:51:35 +0000

    feat: implement password update functionality; add updatePassword method in user service and controller, create UpdatePasswordDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsString({ message: 'O nome deve ser uma string' })
@@ -10,5 +10,6 @@ export class CreateUserDto {
 
   @IsString({ message: 'A senha deve ser uma string' })
   @IsNotEmpty({ message: 'A senha não pode estar vazia' })
+  @MinLength(6, { message: 'Senha deve ter no minimo 6 caracteres' })
   password: string;
 }
