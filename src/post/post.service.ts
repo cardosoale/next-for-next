7b7a4b9714284e3f767f1af095f8fe1ea2b5618f
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2025-09-23 16:37:52 +0000

    feat: enhance post service and controller; implement error handling, slug generation, and response DTOs

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,30 +1,100 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
 import { Repository } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { User } from 'src/user/entities/user.entity';
-import { PostResponseDto } from './dto/post-response.dto';
+import { createSlugFromText } from 'src/common/utils/create-slug-from-text';
 
 @Injectable()
 export class PostService {
+  private readonly logger = new Logger(PostService.name);
+
   constructor(
     @InjectRepository(Post)
     private readonly postRepository: Repository<Post>,
   ) {}
 
-  async create(dto: CreatePostDto, author: User): Promise<PostResponseDto> {
-    const post = this.postRepository.create({
-      slug: 'kfeufhnfuhuv' + Math.random().toString(36).substring(2, 8),
-      title: dto.title,
-      excerpt: dto.excerpt,
-      content: dto.content,
-      coverImageUrl: dto.coverImageUrl,
-      author,
+  async findOneOrfail(postData: Partial<Post>) {
+    const post = await this.findOne(postData);
+
+    if (!post) {
+      throw new NotFoundException('Post não encontrado');
+    }
+
+    return post;
+  }
+
+  async findOne(postData: Partial<Post>) {
+    const post = await this.postRepository.findOne({
+      where: postData,
+      relations: ['author'],
     });
 
-    const created = await this.postRepository.save(post);
+    return post;
+  }
 
-    return new PostResponseDto(created);
+  async findOneOwnedOrFail(postData: Partial<Post>, author: User) {
+    const post = await this.findOneOwned(postData, author);
+
+    if (!post) {
+      throw new NotFoundException('Post não encontrado');
+    }
+
+    return post;
+  }
+
+  async findOneOwned(postData: Partial<Post>, author: User) {
+    const post = await this.postRepository.findOne({
+      where: {
+        ...postData,
+        author: { id: author.id },
+      },
+      relations: ['author'],
+    });
+
+    return post;
+  }
+
+  async findAllOwned(author: User) {
+    const posts = await this.postRepository.find({
+      where: {
+        author: { id: author.id },
+      },
+      order: {
+        createdAt: 'DESC',
+      },
+      relations: ['author'],
+    });
+
+    return posts;
+  }
+
+  async create(dto: CreatePostDto, author: User) {
+    const post = this.postRepository.create({
+      slug: createSlugFromText(dto.title),
+      author,
+      content: dto.content,
+      excerpt: dto.excerpt,
+      coverImageUrl: dto.coverImageUrl,
+      title: dto.title,
+    });
+
+    const created = await this.postRepository
+      .save(post)
+      .catch((err: unknown) => {
+        if (err instanceof Error) {
+          this.logger.error('Erro ao criar post', err.stack);
+        }
+
+        throw new BadRequestException('Erro ao criar o post');
+      });
+
+    return created;
   }
 }
